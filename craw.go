401580d6630c82f4b1c219143b6d99b2da253bff
Mohammad Aziz <mdaziz067@gmail.com>
2019-01-30 13:04:16 +0530

    Limit maximun number of concurrent crawls to 20

--- craw.go
+++ craw.go
@@ -7,9 +7,13 @@ import (
 	"github.com/iAziz786/go-web-crawler/links"
 )
 
+var tokens = make(chan struct{}, 20)
+
 func crawl(url string) []string {
 	fmt.Println(url)
+	tokens <- struct{}{}
 	list, err := links.Extract(url)
+	<-tokens
 	if err != nil {
 		log.Print(err)
 	}
