aca565345e88b8bfc000873abb8b2ad5b8337d76
Mohammad Aziz <mdaziz067@gmail.com>
2019-01-30 13:04:36 +0530

    Exit program when all links are fetched

--- main.go
+++ main.go
@@ -6,14 +6,19 @@ import (
 
 func main() {
 	worklist := make(chan []string)
+	// Number of pending sends to the worklist
+	var n int
 
+	n++
 	go func() { worklist <- os.Args[1:] }()
 	// Crawl the web concurrently.
 	seen := make(map[string]bool)
-	for list := range worklist {
+	for ; n > 0; n-- {
+		list := <-worklist
 		for _, link := range list {
 			if !seen[link] {
 				seen[link] = true
+				n++
 				go func(link string) {
 					worklist <- crawl(link)
 				}(link)
