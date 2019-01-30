6db47f0292a64ea9979e9300202ce16c71259927
Mohammad Aziz <mdaziz067@gmail.com>
2019-01-30 13:28:17 +0530

    Do not crawl urls whose hostname does not belongs to the provided urls

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"net/url"
 	"os"
 )
 
@@ -10,19 +11,33 @@ func main() {
 	var n int
 
 	n++
+	crawlableURLs := os.Args[1:]
 	go func() { worklist <- os.Args[1:] }()
 	// Crawl the web concurrently.
 	seen := make(map[string]bool)
 	for ; n > 0; n-- {
 		list := <-worklist
 		for _, link := range list {
-			if !seen[link] {
-				seen[link] = true
-				n++
-				go func(link string) {
-					worklist <- crawl(link)
-				}(link)
+			if didHostnameMatched(crawlableURLs, link) {
+				if !seen[link] {
+					seen[link] = true
+					n++
+					go func(link string) {
+						worklist <- crawl(link)
+					}(link)
+				}
 			}
 		}
 	}
+}
+
+func didHostnameMatched(urls []string, link string) bool {
+	parsedCandidatedURL, _ := url.Parse(link)
+	for _, eachURL := range urls {
+		parsedURL, _ := url.Parse(eachURL)
+		if parsedURL.Hostname() == parsedCandidatedURL.Hostname() {
+			return true
+		}
+	}
+	return false
 }
